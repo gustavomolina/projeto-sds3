07fc8776882b1476ee8aa33e035d13ed43eed83f
Gustavo <gustavo.souza@accurate.com.br>
2021-05-04 22:37:37 -0300

    Basic static components

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/gustavomolina" target="_blank" rel="noreferrer">Gustavo Molina</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+            Evento promovido pela escola DevSuperior: <a href="https://instagram.com/gustavo.molina96" target="_blank" rel="noreferrer">@gustavo.molina96</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
