fb61a9ef75c9f0377ab41529ecc91298f065c7d4
Gustavo <gustavo.souza@accurate.com.br>
2021-05-09 20:53:14 -0300

    Fix: Commit adjustments

--- frontend/src/components/DataTable/index.tsx
+++ frontend/src/components/DataTable/index.tsx
@@ -23,7 +23,7 @@ const DataTable = () => {
             .then(response => {
                 setPage(response.data)
             })
-    }, []);
+    }, [activePage]);
 
 
     const changePage = (index: number) => {
