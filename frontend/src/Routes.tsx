ac789d90958d705e216396130720676adaac83a2
Gustavo <gustavo.souza@accurate.com.br>
2021-05-09 17:57:33 -0300

    Home, navigation

---
+++ frontend/src/Routes.tsx
@@ -0,0 +1,19 @@
+import DashBoard from 'pages/Dashboard';
+import Home from 'pages/Home';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+const Routes = () => {
+    return (
+        <BrowserRouter>
+        <Switch>
+            <Route path="/" exact>
+                <Home />
+            </Route>
+            <Route path="/dashboard" exact>
+                <DashBoard />
+            </Route>
+        </Switch>
+        </BrowserRouter>
+    );
+}
+
+export default Routes;
